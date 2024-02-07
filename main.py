26d0b3adf9ba2db4f3b97f97a44eb53e6d1cc0ac
fxkurou <118657685+fxkurou@users.noreply.github.com>
2024-02-07 17:34:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,84 +1,68 @@
+import json
 import requests
 from bs4 import BeautifulSoup
-import json
 
-def get_data(url):
+# url = 'https://realpython.github.io/fake-jobs/'
+#
+# headers = {
+#    ' Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0'
+# }
+#
+# response = requests.get(url)
+# src = response.text
+# print(src)
+#
+# with open('index.html', 'w') as file:
+#     file.write(src)
 
-    headers = {
-        "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101Firefox/123.0"
-    }
+with open('index.html') as file:
+    src = file.read()
 
-    # response = requests.get(url, headers)
-    #
-    # with open("projects.html", "w") as file:
-    #     file.write(response.text)
+soup = BeautifulSoup(src, 'html.parser')
+all_jobs = soup.find_all(class_='card-footer-item', string='Apply')
+all_titles = soup.find_all(class_='title is-5')
 
-    with open("projects.html") as file:
-        src = file.read()
+all_vacancies_dict = {}
 
-    soup = BeautifulSoup(src, 'html.parser')
-    cards = soup.find_all('div', class_='card-content')
+for title, job in zip(all_titles, all_jobs):
+    title_text = title.text
+    job_href = job.get('href')
+    all_vacancies_dict[title_text] = job_href
 
-    project_cards = []
+with open('all_vacancies.json') as file:
+    # json.dump(all_vacancies_dict, file, indent=4, ensure_ascii=False)
+    all_vacancies = json.load(file)
 
-    for card in cards:
-        project_url = card.find('a', class_='card-footer-item', string='Apply').get('href')
-        project_cards.append(project_url)
 
-    project_data_list = []
+# all_jobs_list = []
+# for item in all_jobs:
+#     item_text = item.text
+#     item_href = item.get('href')
+#     if item_text == 'Learn': continue
+#     all_jobs_list.append(item_href)
+#
+# with open('all_jobs_list.txt', 'w') as file:
+#     for item in all_jobs_list:
+#         file.write("%s\n" % item)
+#
+# print(all_jobs_list)
 
-    for project_card in project_cards:
-        req = requests.get(project_card, headers)
-        project_name = project_card.split('/')[-1]
+def search(dictionary, key_to_find):
+    found_items = []
 
-        with open(f'data/{project_name}', 'w') as file:
-            file.write(req.text)
+    for key, value in dictionary.items():
+        if key == key_to_find:
+            found_items.append(value)
+        elif isinstance(value, dict):
+            found_items.extend(search(value, key_to_find))
 
-        with open(f'data/{project_name}') as file:
-            src = file.read()
-
-        soup = BeautifulSoup(src, 'html.parser')
-        project_data = soup.find('section', class_='section')
-
-        try:
-            project_title = project_data.find('h1', class_='title is-2').text
-        except Exception:
-            project_title = 'No title'
-
-        try:
-            project_company = project_data.find('h2', class_='subtitle is-4 company').text
-        except Exception:
-            project_company = 'No company'
-
-        try:
-            project_description = project_data.find('div', class_='content').find('p').text
-        except Exception:
-            project_description = 'No description'
-
-        try:
-            project_location = project_data.find('div', class_='content').find('p', id='location').text.split(':')[-1].strip()
-        except Exception:
-            project_location = 'No location'
-
-        try:
-            project_post_time = project_data.find('div', class_='content').find('p', id='date').text.split(':')[-1].strip()
-        except Exception:
-            project_post_time = 'No post time'
-
-        project_data_list.append(
-            {
-                'Vacancy name: ': project_title,
-                'Vacancy company: ': project_company,
-                'Vacancy description: ': project_description,
-                'Vacancy location: ': project_location,
-                'Vacancy post date: ': project_post_time
-            }
-        )
-
-    with open('data/projects_data.json', 'a', encoding='utf-8') as file:
-        json.dump(project_data_list, file, indent=4)
+    return found_items
 
 def main():
-    get_data("https://realpython.github.io/fake-jobs/")
-if __name__=='__main__':
+    inp = input("Enter vacancie name: ")
+    res = search(all_vacancies, inp)
+    print('Result: ', ' '.join(res))
+
+if __name__ == "__main__":
     main()
